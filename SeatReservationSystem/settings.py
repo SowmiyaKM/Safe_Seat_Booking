4eba7fd84fce3db4d16fbe6b39b4933558f6ee09
SowmiyaKM <sowmiyakmk@gmail.com>
2026-06-24 22:12:53 +0530

    Prepare for Render deployment

--- SeatReservationSystem/settings.py
+++ SeatReservationSystem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-39r6u5so^(-b$^98oe^q6g#fpjz$%f$#1hdv+u1_bclmy4rvn6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS=[
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,3 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 LOGIN_URL='login'
+
+STATIC_ROOT=BASE_DIR/'staticfiles'
+
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
